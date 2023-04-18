795410cc025a87fc1e7d0ecf9a570e3b7d5ed049
jarli <jarli@DENODEN>
2023-04-18 18:35:35 +0200

    Добавьте файлы проекта.

---
+++ DZ_1/DZ_1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //1
+    cout << "\tApril 2023\n";
+    cout << "Mon Tue Wed Thu Fri Sat Sun\n";
+    cout << " 27  28  29  30  31   1   2\n";
+    cout << "  3   4   5   6   7   8   9\n";
+    cout << " 10  11  12  13  14  15  16\n";
+    cout << " 17  18  19  20  21  22  23\n";
+    cout << " 24  25  26  27  28  29  30\n\n\n";
+
+    //2
+    cout << "    /\\_   \n";
+    cout << "   /  \\|  \n";
+    cout << "  /    \\  \n";
+    cout << "  |    |   \n";
+    cout << "  |__  |   \n";
+    cout << "  |__| |   \n";
+    cout << "  |  __| /\\ /\\ /\\ /\\ \n";
+    cout << "  |  |_|=||=||=||=||  \n";
+    cout << "  |__|_| || || || ||  \n\n\n";
+
+    //3
+    cout << "  **********    *********  ***      ***  ***    *****       *****   \n";
+    cout << "  **********    *********  ***      ***  ***   ** ***     ********* \n";
+    cout << "  ***    ***    ***        ***      ***  ***   ** ***    ***     ***\n";
+    cout << "  ***    ***    ***        ***      ***  ***   ** ***   ***         \n";
+    cout << "  ***    ***    *********  ************  ***  **  ***   ***         \n";
+    cout << "  ***    ***    *********  ************  ***  **  ***   ***         \n";
+    cout << "  **********    ***        ***      ***  *** **   ***   ***         \n";
+    cout << "**************  ***        ***      ***  *** **   ***    ***     ***\n";
+    cout << "****      ****  *********  ***      ***  *** **   ***     ********* \n";
+    cout << "****      ****  *********  ***      ***  *****    ***       *****   \n\n\n";
+
+    //4
+    cout << " *\n";
+    cout << "\t*\n\n\n";
+    cout << "\t\t*\n\n";
+    cout << "\t\t\t*\t\t\t    *\n\n\n\n\n";
+    cout << "\t\t\t   *\t\t\t*\n\n\n";
+
+    //5
+    cout << "\tYaroshenko Denys\n";
+    cout << "\t Odesa, Ukraine\n";
+    cout << "\t    17 years\n";
+    cout << "   Study at \"Step\" academy\n";
+    cout << "   I want to find a good job\n";
+    cout << "   Programming languages: python, c++\n";
+}
